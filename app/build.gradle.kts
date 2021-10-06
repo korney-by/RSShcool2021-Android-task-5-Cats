d440338e8a94d6fde810adf5b37237064ad5f0d3
Sergey Kornienko <korney.by@gmail.com>
2021-10-07 01:45:21 +0300

    feat: Gradle format moved to KTS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,18 @@ plugins {
     kotlin("kapt")
 }
 
-android {
-    compileSdk(31)
+repositories {
+    google()
+    mavenCentral()
+}
+
+android  {
+    compileSdk = 31
 
     defaultConfig {
         applicationId = "com.korneysoft.rsshcool2021_android_task_5_cats"
-        minSdk(21)
-        targetSdk(31)
+        minSdk = 21
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
@@ -18,7 +23,7 @@ android {
     }
 
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -44,7 +49,8 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+    val kotlinVersion = "1.5.31"
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
 
@@ -63,11 +69,11 @@ dependencies {
     implementation("com.squareup.moshi:moshi:$moshiVersion")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
     implementation("androidx.paging:paging-runtime-ktx:3.0.1")
-    implementation("com.squareup.okhttp3:okhttp:3.14.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.1")
 
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,23 @@
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("com.android")) {
+                useModule("com.android.tools.build:gradle:7.0.2")
+            }
+            if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
+                useVersion("1.5.21")
+            }
+//            if (requested.id.id.startsWith("dagger.hilt.android")) {
+//                useModule("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+//            }
+        }
     }
 }
+
 rootProject.name = "RSShcool2021-Android-task-5-Cats"
 include (":app")
